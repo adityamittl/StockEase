13121e707ddce6337a157e73ca934b4d9d054b3c
Aditya Mittal <aditmttl@gmail.com>
2023-01-12 14:12:42 +0530

    Store Database Schema

---
+++ assetsData/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+
+# Database schema for the initial populated data 
+
+# ------------------ Item anem --------------------
+# specifing the asset super catagory
+class Main_Catagory(models.Model):
+    Consumable_type = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# To specify the subcatagory of the asset type
+class Sub_Catagory(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.IntegerField()
+
+    def __self__(self):
+        return self.name
+
+# to specify the asset type
+class Asset_Type(models.Model):
+
+    name = models.CharField(max_length=500)
+    code = models.IntegerField()
+    Final_Code = models.CharField(max_length=200)
+    Last_Assigned_serial_Number = models.IntegerField(default=0)
+
+    def __self__(self):
+        return self.name
+
+
+# ------------ Location Master ---------------------
+
+# Floor and its corrosponding code
+
+class Floor_Code(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Building_Name(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Location_Description(models.Model):
+    name = models.CharField(max_length=500)
+    code = models.CharField(max_length=100)
+    floor = models.ForeignKey(Floor_Code, on_delete=models.CASCADE)
+    Final_Code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# ------------ Departments and labs name --------------
+class Departments(models.model):
+    name = models.CharField(max_length=300)
+
+
+# -------------- Vendor Database -----------------
+class Service_Type(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor_Attachments(models.Model):
+    File_Name = models.CharField(max_length=500)
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField(null=True, blank=True)
+    GST_No = models.CharField(max_length=100, null=True, blank=True)
+    contact_No = models.CharField(max_length=100, null=True, blank=True)
+    services = models.ManyToManyField(Service_Type, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
